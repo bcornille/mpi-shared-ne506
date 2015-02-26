474d39fba75856b23c9c6183fe842f1c58a2b627
bcornille <bcornille7@gmail.com>
2015-02-25 21:49:53 -0600

    This basic Hello World Program appears to be working.  It would be prudent to test this program on a distributed system such as the CHTC/ACI HPC cluster.

--- mpi_shmem_PoC/mpi_shmem_HelloWorld.c
+++ mpi_shmem_PoC/mpi_shmem_HelloWorld.c
@@ -1,23 +1,23 @@
 #include <stdio.h>
 #include "mpi.h"
 
-int main(int argc, char **argv[])
+int main(int argc, char *argv[])
 {
 	// This block initializes MPI and defines rank and size in the global communicator.
 	MPI_Init(&argc, &argv);
 	int r, s;
-	MPI_comm comm = MPI_COMM_WORLD;
+	MPI_Comm comm = MPI_COMM_WORLD;
 	MPI_Comm_rank(comm, &r);
 	MPI_Comm_size(comm, &s);
 
 	// This block defines rankd and size in the shared memory communicator.
-	MPI_comm shmcomm;
+	MPI_Comm shmcomm;
 	MPI_Comm_split_type(comm, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &shmcomm);
 	int sr, ss;
 	MPI_Comm_size(shmcomm, &ss);
-	MPI_Comm_rank(shmcomm, &rs);
+	MPI_Comm_rank(shmcomm, &sr);
 
-	printf("Hello from processor %d.  I have rank %d in my shared memory group.", &r, &rs);
+	printf("Hello from processor %d.  I have rank %d in my shared memory group.\n", r, sr);
 
 	MPI_Finalize();
 	return 0;
