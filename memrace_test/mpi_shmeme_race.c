b67425c83f3afc53c324aba253c9ad9f33c50fe1
bcornille <bcornille7@gmail.com>
2015-03-04 10:39:33 -0600

    More work on mpi_shmem_race.c.

--- memrace_test/mpi_shmeme_race.c
+++ memrace_test/mpi_shmeme_race.c
@@ -3,8 +3,14 @@
 
 int main(int argc, char *argv[])
 {
-	// This block initializes MPI and defines rank and size in the global communicator.
+	// This block initializes MPI and gets the processor rank in the global communicator.
 	MPI_Init(&argc, &argv);
 	int rank;
 	MPI_Comm comm = MPI_COMM_WORLD;
 	MPI_Comm_rank(comm, &rank);
+
+	// Set up the shared memory communicator and get shared memory rank.
+	MPI_Comm shmcomm;
+	MPI_Comm_split_type(comm, MPI_COMM_TYPE_SHARED, 0, MPI_INFO_NULL, &shmcomm);
+	int srank;
+	MPI_Comm_rank(shmcomm, &srank);
