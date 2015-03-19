d7e38d45d037e15674fd61fa6413846e76b32c82
bcornille <bcornille7@gmail.com>
2015-03-18 19:33:02 -0500

    Started work on the actual Monte Carlo program.

---
+++ src/globals.h
@@ -0,0 +1,9 @@
+#ifndef _globals_h
+#define _globals_h
+
+// MPI related variables.
+int rank	// Process rank
+int srank	// Process rank in shared memory communicator
+int ssize	// Size of shared memory communicator
+
+#endif
